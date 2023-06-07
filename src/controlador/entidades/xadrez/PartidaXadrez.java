621706e73d6bcc2cbfdca7458a51da5c657fa50d
Felipe Silva Navarro <felipenavarro07@hotmail.com>
2023-06-07 19:40:31 -0300

    Movimentos possiveis do rei

--- src/controlador/entidades/xadrez/PartidaXadrez.java
+++ src/controlador/entidades/xadrez/PartidaXadrez.java
@@ -4,6 +4,7 @@ import controlador.entidades.tabuleiro.Peca;
 import controlador.entidades.tabuleiro.Posicao;
 import controlador.entidades.tabuleiro.Tabuleiro;
 import controlador.entidades.xadrez.excecoes.XadrezExcecoes;
+import controlador.entidades.xadrez.pecas.Rei;
 import controlador.entidades.xadrez.pecas.Torre;
 
 public class PartidaXadrez {
@@ -87,16 +88,16 @@ public class PartidaXadrez {
 
     private void iniciarSetup() {
         colocarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
-        colocarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
-        colocarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('c', 2, new Rei(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('d', 2, new Rei(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
-        colocarNovaPeca('d', 1, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
 
         colocarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
         colocarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
         colocarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
-        colocarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('e', 7, new Rei(tabuleiro, Cor.PRETO));
         colocarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
         colocarNovaPeca('d', 8, new Torre(tabuleiro, Cor.PRETO));
     }
--- src/controlador/entidades/xadrez/pecas/Rei.java
+++ src/controlador/entidades/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package controlador.entidades.xadrez.pecas;
 
+import controlador.entidades.tabuleiro.Posicao;
 import controlador.entidades.tabuleiro.Tabuleiro;
 import controlador.entidades.xadrez.Cor;
 import controlador.entidades.xadrez.PecaXadrez;
@@ -14,9 +15,62 @@ public class Rei extends PecaXadrez {
         return "R";
     }
 
+    /**
+     * @param posicao
+     * @return <p>Verdadeiro se pode mover para a posicao passada como paramentro</p>
+     */
+    private boolean podeMover(Posicao posicao) {
+        PecaXadrez p = (PecaXadrez) getTabuleiro().peca(posicao);
+        return p == null || p.getCor() != getCor();
+    }
+
     @Override
     public boolean[][] possiveisMovimentos() {
-        boolean[][] aux = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
-        return aux;
+        boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+        Posicao p = new Posicao(0, 0);
+
+        // Acima
+        p.setValores(posicao.getLinha() - 1, posicao.getColuna());
+        if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        // Abaixo
+        p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+        if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        // Esquerda
+        p.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        // Direita
+        p.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        // NO
+        p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        // NE
+        p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        // SO
+        p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        // SE
+        p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        return mat;
     }
 }
