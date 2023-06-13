5d3bbe457ee4d0947fd51a2c68c92dd65e25d8b6
Felipe Silva Navarro <felipenavarro07@hotmail.com>
2023-06-13 20:56:15 -0300

    Cavalo

--- src/controlador/entidades/xadrez/PartidaXadrez.java
+++ src/controlador/entidades/xadrez/PartidaXadrez.java
@@ -4,10 +4,7 @@ import controlador.entidades.tabuleiro.Peca;
 import controlador.entidades.tabuleiro.Posicao;
 import controlador.entidades.tabuleiro.Tabuleiro;
 import controlador.entidades.xadrez.excecoes.XadrezExcecoes;
-import controlador.entidades.xadrez.pecas.Bispo;
-import controlador.entidades.xadrez.pecas.Peao;
-import controlador.entidades.xadrez.pecas.Rei;
-import controlador.entidades.xadrez.pecas.Torre;
+import controlador.entidades.xadrez.pecas.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -230,9 +227,11 @@ public class PartidaXadrez {
 
     private void iniciarSetup() {
         colocarNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('a', 2, new Peao(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('b', 2, new Peao(tabuleiro, Cor.BRANCO));
@@ -244,9 +243,11 @@ public class PartidaXadrez {
         colocarNovaPeca('h', 2, new Peao(tabuleiro, Cor.BRANCO));
 
         colocarNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
         colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETO));
         colocarNovaPeca('a', 7, new Peao(tabuleiro, Cor.PRETO));
         colocarNovaPeca('b', 7, new Peao(tabuleiro, Cor.PRETO));
